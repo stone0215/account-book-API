503e83e2338aa230594051d6e1490cc38f410a58
catann <st89b@hotmail.com>
2021-03-06 13:24:05 +0800

    adjust insurance model

---
+++ app/dao/model/otherAsset/insurance_model.py
@@ -0,0 +1,124 @@
+from datetime import datetime, timedelta
+from sqlalchemy import asc
+import requests
+import json
+
+from ...dao_base import DaoBase
+
+db = DaoBase().getDB()
+
+
+class Insurance(db.Model):
+    __tablename__ = 'Insurance'
+    insurance_id = db.Column(db.Integer, primary_key=True)
+    insurance_name = db.Column(db.String(60), nullable=False)
+    asset_id = db.Column(db.Integer, nullable=False)
+    in_account_id = db.Column(db.Integer, nullable=False)
+    in_account_name = db.Column(db.String(60), nullable=False)
+    out_account_id = db.Column(db.Integer, nullable=False)
+    out_account_name = db.Column(db.String(60), nullable=False)
+    start_date = db.Column(db.DateTime, nullable=False, index=True)
+    expected_end_date = db.Column(db.DateTime, nullable=False)
+    pay_type = db.Column(db.String(10), nullable=False)
+    pay_day = db.Column(db.String(23), nullable=False)
+    expected_spend = db.Column(db.Float, nullable=False)
+    has_closed = db.Column(db.String(1), nullable=False)
+
+    # 物件建立之後所要建立的初始化動作
+    def __init__(self, insurance_name, asset_id, in_account_id, in_account_name, out_account_id, out_account_name, start_date, expected_end_date, pay_type, pay_day, expected_spend, has_closed):
+        self.insurance_name = insurance_name
+        self.asset_id = asset_id
+        self.in_account_id = in_account_id
+        self.in_account_name = in_account_name
+        self.out_account_id = out_account_id
+        self.out_account_name = out_account_name
+        self.start_date = start_date
+        self.expected_end_date = expected_end_date
+        self.pay_type = pay_type
+        self.pay_day = pay_day
+        self.expected_spend = expected_spend if expected_spend else None
+        self.has_closed = has_closed
+
+    # 定義物件的字串描述，執行 print(x) 就會跑這段
+    def __str__(self):
+        return self
+
+    def queryByKey(self, insurance_id):
+        return self.query.filter_by(insurance_id=insurance_id).first()
+
+    def query4Summary(self, asset_id):
+        sql = []
+        sql.append(
+            "SELECT insurance_main.insurance_id, insurance_name, asset_id, in_account_id, in_account_name, out_account_id, out_account_name, ")
+        sql.append(
+            "pay_type, pay_day, start_date, expected_end_date, has_closed, expected_spend, Insurance_Amount.actual_spend, ")
+        sql.append(
+            "Insurance_Profit.gain_lose, ROUND(100*Insurance_Profit.gain_lose/Insurance_Amount.actual_spend,2) AS ROI ")
+        sql.append("FROM Insurance insurance_main ")
+        sql.append(
+            "LEFT JOIN (SELECT insurance_id, insurance_excute_type, SUM(IFNULL(excute_price,0)) AS gain_lose ")
+        sql.append("    FROM Insurance_Journal) Insurance_Profit ")
+        sql.append(
+            "    ON Insurance_Profit.insurance_id=insurance_main.insurance_id ")
+        sql.append(
+            "        AND (Insurance_Profit.insurance_excute_type = 'cash' OR Insurance_Profit.insurance_excute_type = 'return')")
+        sql.append(
+            "LEFT JOIN (SELECT insurance_id, insurance_excute_type, SUM(IFNULL(excute_price,0)) AS actual_spend ")
+        sql.append("    FROM Insurance_Journal) Insurance_Amount ")
+        sql.append(
+            "    ON Insurance_Amount.insurance_id=insurance_main.insurance_id")
+        sql.append("        AND Insurance_Amount.insurance_excute_type = 'pay'")
+        sql.append("WHERE asset_id=" + str(asset_id))
+        sql.append(" ORDER BY insurance_main.insurance_id ASC")
+
+        return db.engine.execute(''.join(sql))  # sql 陣列轉字串
+
+    def add(self, insurance):
+        db.session.add(insurance)
+        db.session.flush()
+
+        if DaoBase.session_commit(self) == '':
+            return insurance
+        else:
+            return False
+
+    def update(self):
+        if DaoBase.session_commit(self) == '':
+            return True
+        else:
+            return False
+
+    def delete(self, insurance_id):
+        self.query.filter_by(insurance_id=insurance_id).delete()
+        if DaoBase.session_commit(self) == '':
+            return True
+        else:
+            return False
+
+    def output4View(self, insurance):
+        return {
+            'insurance_id': insurance.insurance_id,
+            'insurance_name': insurance.insurance_name,
+            'asset_id': insurance.asset_id,
+            'in_account_id': insurance.in_account_id,
+            'in_account_name': insurance.in_account_name,
+            'out_account_id': insurance.out_account_id,
+            'out_account_name': insurance.out_account_name,
+            'pay_type': insurance.pay_type,
+            'pay_day': insurance.pay_day,
+            'expected_spend': insurance.expected_spend,
+            'start_date': datetime.strptime(insurance.start_date, '%Y-%m-%d %H:%M:%S.%f') if insurance.start_date else '',
+            'expected_end_date': datetime.strptime(insurance.expected_end_date, '%Y-%m-%d %H:%M:%S.%f') if insurance.expected_end_date else '',
+            'has_closed': insurance.has_closed,
+            'actual_spend': insurance.actual_spend,
+            'gain_lose': insurance.gain_lose,
+            'ROI': insurance.ROI
+            # 'IRR': insurance.IRR
+        }
+
+    def output(self, insurance):
+        return {
+            'insurance_id': insurance.insurance_id,
+            'insurance_name': insurance.insurance_name,
+            'asset_id': insurance.asset_id
+        }
--- router/otherAsset/insuranceAssetRouter.py
+++ router/otherAsset/insuranceAssetRouter.py
@@ -4,8 +4,8 @@ from datetime import datetime
 from flask import jsonify, request
 
 from api.response_format import ResponseFormat
-from app.dao.model.otherAsset.insurance_detail_model import InsuranceDetail
 from app.dao.model.otherAsset.insurance_journal_model import InsuranceJournal
+from app.dao.model.otherAsset.insurance_model import Insurance
 
 date_format = '%Y-%m-%dT%H:%M:%S.%fZ'
 
@@ -16,11 +16,11 @@ def init_insurance_asset_api(app):
         output = []
 
         try:
-            insurance_assets = InsuranceJournal.query4Summary(
-                InsuranceJournal, asset_id)
+            insurance_assets = Insurance.query4Summary(
+                Insurance, asset_id)
             for insurance_asset in insurance_assets:
-                output.append(InsuranceJournal.output4View(
-                    InsuranceJournal, insurance_asset))
+                output.append(Insurance.output4View(
+                    Insurance, insurance_asset))
         except Exception as error:
             return jsonify(ResponseFormat.false_return(ResponseFormat, error))
         else:
@@ -34,19 +34,19 @@ def init_insurance_asset_api(app):
             # force=True 忽略mimetype，只接字串
             inputData = request.get_json(force=True)
 
-            insurance_asset = InsuranceJournal(asset_id=inputData['asset_id'], insurance_name=inputData['insurance_name'],
-                                               start_date=datetime.strptime(
-                                                   inputData['start_date'], date_format),
-                                               expected_end_date=datetime.strptime(
-                                                   inputData['expected_end_date'], date_format),
-                                               pay_type=inputData['pay_type'], pay_day=inputData['pay_day'],
-                                               in_account_id=inputData['in_account_id'], in_account_name=inputData['in_account_name'],
-                                               out_account_id=inputData['out_account_id'], out_account_name=inputData['out_account_name'],
-                                               expected_spend=inputData['expected_spend'], has_closed=inputData['has_closed'])
-            result = InsuranceJournal.add(InsuranceJournal, insurance_asset)
+            insurance_asset = Insurance(asset_id=inputData['asset_id'], insurance_name=inputData['insurance_name'],
+                                        start_date=datetime.strptime(
+                inputData['start_date'], date_format),
+                expected_end_date=datetime.strptime(
+                inputData['expected_end_date'], date_format),
+                pay_type=inputData['pay_type'], pay_day=inputData['pay_day'],
+                in_account_id=inputData['in_account_id'], in_account_name=inputData['in_account_name'],
+                out_account_id=inputData['out_account_id'], out_account_name=inputData['out_account_name'],
+                expected_spend=inputData['expected_spend'], has_closed=inputData['has_closed'])
+            result = Insurance.add(Insurance, insurance_asset)
 
             if result:
-                return jsonify(ResponseFormat.true_return(ResponseFormat, InsuranceJournal.output(InsuranceJournal, result)))
+                return jsonify(ResponseFormat.true_return(ResponseFormat, Insurance.output(Insurance, result)))
             else:
                 return jsonify(ResponseFormat.false_return(ResponseFormat, None, 'fail to add insurance data'))
         except Exception as error:
@@ -55,8 +55,8 @@ def init_insurance_asset_api(app):
     @app.route('/other-asset/insurance/<int:insurance_id>', methods=['PUT'])
     def updateInsuranceAsset(insurance_id):
         try:
-            insurance_asset = InsuranceJournal.queryByKey(
-                InsuranceJournal, insurance_id)
+            insurance_asset = Insurance.queryByKey(
+                Insurance, insurance_id)
             if insurance_asset is None:
                 return jsonify(ResponseFormat.false_return(ResponseFormat, None, 'data not found'))
             else:
@@ -70,7 +70,7 @@ def init_insurance_asset_api(app):
                 insurance_asset.out_account_id = inputData['out_account_id']
                 insurance_asset.out_account_name = inputData['out_account_name']
                 # insurance_asset.expected_spend = inputData['expected_spend'] if inputData['expected_spend'] else None
-                if InsuranceJournal.update(InsuranceJournal):
+                if Insurance.update(Insurance):
                     return jsonify(ResponseFormat.true_return(ResponseFormat, None))
                 else:
                     return jsonify(ResponseFormat.false_return(ResponseFormat, None, 'fail to update insurance data'))
@@ -80,12 +80,12 @@ def init_insurance_asset_api(app):
     @app.route('/other-asset/insurance/<int:insurance_id>', methods=['DELETE'])
     def deleteInsuranceAsset(insurance_id):
         try:
-            insurance_asset = InsuranceJournal.queryByKey(
-                InsuranceJournal, insurance_id)
+            insurance_asset = Insurance.queryByKey(
+                Insurance, insurance_id)
             if insurance_asset is None:
                 return jsonify(ResponseFormat.false_return(ResponseFormat, None, 'data not found'))
             else:
-                if InsuranceJournal.delete(InsuranceJournal, insurance_id):
+                if Insurance.delete(Insurance, insurance_id):
                     return jsonify(ResponseFormat.true_return(ResponseFormat, None))
                 else:
                     return jsonify(ResponseFormat.false_return(ResponseFormat, None, 'fail to delete insurance data'))
@@ -93,60 +93,60 @@ def init_insurance_asset_api(app):
             return jsonify(ResponseFormat.false_return(ResponseFormat, error))
 
     @app.route('/other-asset/insurance/detail/<int:insurance_id>', methods=['GET'])
-    def getInsuranceDetails(insurance_id):
+    def getInsuranceJournals(insurance_id):
         output = []
 
         try:
-            insurance_details = InsuranceDetail.queryByInsuranceId(
-                InsuranceDetail, insurance_id)
-            for insurance_detail in insurance_details:
-                output.append(InsuranceDetail.output(
-                    InsuranceDetail, insurance_detail))
+            insurance_journals = InsuranceJournal.queryByInsuranceId(
+                InsuranceJournal, insurance_id)
+            for insurance_journal in insurance_journals:
+                output.append(InsuranceJournal.output(
+                    InsuranceJournal, insurance_journal))
         except Exception as error:
             return jsonify(ResponseFormat.false_return(ResponseFormat, error))
         else:
             return jsonify(ResponseFormat.true_return(ResponseFormat, output))
 
     @app.route('/other-asset/insurance/detail', methods=['POST'])
-    def addInsuranceDetail():
+    def addInsuranceJournal():
         global date_format
 
         try:
             # force=True 忽略mimetype，只接字串
             inputData = request.get_json(force=True)
 
-            insurance_detail = InsuranceDetail(insurance_id=inputData['insurance_id'], insurance_excute_type=inputData['insurance_excute_type'],
-                                               excute_price=inputData['excute_price'], excute_date=datetime.strptime(
-                                                   inputData['excute_date'], date_format),
-                                               memo=inputData['memo'])
-            result = InsuranceDetail.add(InsuranceDetail, insurance_detail)
+            insurance_journal = InsuranceJournal(insurance_id=inputData['insurance_id'], insurance_excute_type=inputData['insurance_excute_type'],
+                                                 excute_price=inputData['excute_price'], excute_date=datetime.strptime(
+                inputData['excute_date'], date_format),
+                memo=inputData['memo'])
+            result = InsuranceJournal.add(InsuranceJournal, insurance_journal)
 
             if result:
-                return jsonify(ResponseFormat.true_return(ResponseFormat, InsuranceDetail.output(InsuranceDetail, result)))
+                return jsonify(ResponseFormat.true_return(ResponseFormat, InsuranceJournal.output(InsuranceJournal, result)))
             else:
                 return jsonify(ResponseFormat.false_return(ResponseFormat, None, 'fail to add insurance detail data'))
         except Exception as error:
             return jsonify(ResponseFormat.false_return(ResponseFormat, error))
 
     @app.route('/other-asset/insurance/detail/<int:distinct_number>', methods=['PUT'])
-    def updateInsuranceDetail(distinct_number):
+    def updateInsuranceJournal(distinct_number):
         global date_format
 
         try:
-            insurance_detail = InsuranceDetail.queryByKey(
-                InsuranceDetail, distinct_number)
-            if insurance_detail is None:
+            insurance_journal = InsuranceJournal.queryByKey(
+                InsuranceJournal, distinct_number)
+            if insurance_journal is None:
                 return jsonify(ResponseFormat.false_return(ResponseFormat, None, 'data not found'))
             else:
                 inputData = request.get_json(force=True)
 
-                insurance_detail.insurance_excute_type = inputData['insurance_excute_type']
-                insurance_detail.excute_price = inputData['excute_price']
-                insurance_detail.excute_date = datetime.strptime(
+                insurance_journal.insurance_excute_type = inputData['insurance_excute_type']
+                insurance_journal.excute_price = inputData['excute_price']
+                insurance_journal.excute_date = datetime.strptime(
                     inputData['excute_date'], date_format)
-                insurance_detail.memo = inputData['memo']
+                insurance_journal.memo = inputData['memo']
 
-                if InsuranceDetail.update(InsuranceDetail):
+                if InsuranceJournal.update(InsuranceJournal):
                     return jsonify(ResponseFormat.true_return(ResponseFormat, None))
                 else:
                     return jsonify(ResponseFormat.false_return(ResponseFormat, None, 'fail to update insurance detail data'))
@@ -154,14 +154,14 @@ def init_insurance_asset_api(app):
             return jsonify(ResponseFormat.false_return(ResponseFormat, error))
 
     @app.route('/other-asset/insurance/detail/<int:distinct_number>', methods=['DELETE'])
-    def deleteInsuranceDetail(distinct_number):
+    def deleteInsuranceJournal(distinct_number):
         try:
-            insurance_detail = InsuranceDetail.queryByKey(
-                InsuranceDetail, distinct_number)
-            if insurance_detail is None:
+            insurance_journal = InsuranceJournal.queryByKey(
+                InsuranceJournal, distinct_number)
+            if insurance_journal is None:
                 return jsonify(ResponseFormat.false_return(ResponseFormat, None, 'data not found'))
             else:
-                if InsuranceDetail.delete(InsuranceDetail, distinct_number):
+                if InsuranceJournal.delete(InsuranceJournal, distinct_number):
                     return jsonify(ResponseFormat.true_return(ResponseFormat, None))
                 else:
                     return jsonify(ResponseFormat.false_return(ResponseFormat, None, 'fail to delete insurance detail data'))
